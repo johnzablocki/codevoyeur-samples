774c61e0fff14a9a32825917b33f49b14205d4a4
John Zablocki <jzablocki@users.noreply.github.com>
2011-01-27 18:11:39 -0500

    Adding simplegeo WP7 sample and testing precommit hook

---
+++ src/PresentationSamples/WP7Location/SimpleGeoPlaces/Models/Core/Geometry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Prohibition.SimpleGeo.Core;
+
+namespace Prohibition.SimpleGeo.Core {
+    
+    public class Geometry {
+
+        public string Point { get; set; }
+
+        //TODO: convert to list of Coordinate 
+        public IList<decimal> Coordinates { get; set; }        
+
+    }
+}
