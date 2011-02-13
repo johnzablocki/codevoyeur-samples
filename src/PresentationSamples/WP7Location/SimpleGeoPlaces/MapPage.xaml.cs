66bc0bc9746bf7a142d1e05fb3ae8fb84225b628
John Zablocki <jzablocki@users.noreply.github.com>
2011-02-13 17:33:21 -0500

    WP7 map w/ MVVM sample

---
+++ src/PresentationSamples/WP7Location/SimpleGeoPlaces/MapPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Controls.Maps;
+using System.Device.Location;
+using SimpleGeoPlaces.Models.Features;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+
+namespace SimpleGeoPlaces {
+    public partial class Map : PhoneApplicationPage {
+        public Map() {
+            InitializeComponent();
+
+            App.MapPageViewModel.LoadData(PhoneApplicationService.Current.State["SelectedRestaurant"] as Feature);            
+            DataContext = App.MapPageViewModel;
+        }
+
+        void button_Click(object sender, RoutedEventArgs e) {
+            var task = new PhoneCallTask();
+            task.PhoneNumber = App.MapPageViewModel.PhoneNumber;
+            task.Show(); 
+        }
+    
+    }
+}
