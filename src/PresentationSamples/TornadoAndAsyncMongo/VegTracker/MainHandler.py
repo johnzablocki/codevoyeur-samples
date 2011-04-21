67d924b1df82b28dc4eaa4881a6e1d155c30814c
John Zablocki <jzablocki@users.noreply.github.com>
2011-04-21 19:36:07 -0400

    Working CRUD sample, still buggy _id gets

--- src/PresentationSamples/TornadoAndAsyncMongo/VegTracker/MainHandler.py
+++ src/PresentationSamples/TornadoAndAsyncMongo/VegTracker/MainHandler.py
@@ -4,6 +4,8 @@ import tornado.ioloop
 import asyncmongo
 
 from RestaurantsHandler import RestaurantsHandler
+from RestaurantUpdateHandler import RestaurantUpdateHandler
+from RestaurantDeleteHandler import RestaurantDeleteHandler
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -11,16 +13,16 @@ class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 						
 		db = asyncmongo.Client(pool_id="my_db", host="127.0.0.1", port=27017, maxcached=10, maxconnections=50, dbname="VegTracker")
-		db.venues.find(limit=10, callback=self._get_callback)		
-	
-		
+		db.venues.find(limit=10, callback=self._get_callback)					
 	
 	def _get_callback(self, response, error):
 			self.render("templates/Main/Index.html", venues=response)
 	
 application = tornado.web.Application([
 	(r"/", MainHandler),
-	(r"/restaurants/", RestaurantsHandler)
+	(r"/restaurants/create/", RestaurantsHandler),
+	(r"/restaurants/edit/", RestaurantUpdateHandler),
+	(r"/restaurants/delete/", RestaurantDeleteHandler),
 ], debug=True)
 
 if __name__ == "__main__":
--- src/PresentationSamples/TornadoAndAsyncMongo/VegTracker/RestaurantsHandler.py
+++ src/PresentationSamples/TornadoAndAsyncMongo/VegTracker/RestaurantsHandler.py
@@ -4,43 +4,26 @@ import tornado.ioloop
 import asyncmongo
 import pymongo.objectid
 
+from DBFactory import DBFactory
+
 class RestaurantsHandler(tornado.web.RequestHandler):
 
 	@tornado.web.asynchronous
 	def get(self):
 	
-		id = self.get_argument("id", None)
-		
-		if id != None:			
-			
-			spec = { "_id" : pymongo.objectid.ObjectId(id) }
-			
-			db = asyncmongo.Client(pool_id="my_db", host="127.0.0.1", port=27017, maxcached=10, maxconnections=50, dbname="VegTracker")
-			db.venues.find_one(spec, callback=lambda response, error: self.render("templates/Restaurants/Create.html", venue=response[0]))
-		else:		
-			self.render("templates/Restaurants/Create.html", venue={ "name" : "Horizons", "city" : "Philadelphia", "state" : "PA", "_id" : "" })
+		self.render("templates/Restaurants/Create.html", venue={ "name" : "", "city" : "", "state" : "" })
 	
 	@tornado.web.asynchronous
 	def post(self):
 		
-		doc = {}
-		
-		id = self.get_argument("_id", None)
-		
-		if id != None:
-			doc["_id"] = id
+		doc = {}		
 		
 		doc["name"] = self.get_argument("name")
 		doc["city"] = self.get_argument("city")
 		doc["state"] = self.get_argument("state")
 		
-		db = asyncmongo.Client(pool_id="my_db", host="127.0.0.1", port=27017, maxcached=10, maxconnections=50, dbname="VegTracker")
-		
-		if id == None:
-			db.venues.insert(doc, callback=self._post_callback)
-		else:
-			db.venues.update({ "_id" : pymongo.objectid.ObjectId(id) }, doc, callback=self._post_callback)
-				
+		db = DBFactory.create()		
+		db.venues.insert(doc, callback=self._post_callback)			
 		
 	def _post_callback(self, response, error):
 	
