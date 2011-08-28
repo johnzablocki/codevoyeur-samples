e8e48011ea8e012caaf65fb522e290dfd9c7a84b
johnzablocki <johnzablocki@localhost>
2011-08-28 16:59:33 -0400

    MongoDBQuickStart ported to couch in CouchDBQuickStart

--- src/PresentationSamples/NoSQLAndDotNet/MongoDBQuickStart/Program.cs
+++ src/PresentationSamples/NoSQLAndDotNet/MongoDBQuickStart/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using NoRMQuickStart;
+using MongoDBQuickStart;
 using MongoDB.Driver;
 using MongoDB.Bson;
 using MongoDB.Driver.Builders;
