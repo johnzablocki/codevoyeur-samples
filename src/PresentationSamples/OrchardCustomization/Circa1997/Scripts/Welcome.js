6ca7dd8d29c1b9ebecd40d811b472d49e2af7740
johnzablocki <johnzablocki@localhost>
2011-09-26 23:16:51 -0400

    Demo theme and module for NYC Code Camp

---
+++ src/PresentationSamples/OrchardCustomization/Circa1997/Scripts/Welcome.js
@@ -0,0 +1,16 @@
+﻿var fullMessage = "Welcome to Code Camp NYC!";
+
+
+function scroll() {
+    
+    var currentStatus = window.status;    
+    if (currentStatus == fullMessage) {
+        window.status = fullMessage[0];
+    } else {
+        var nextMessage = fullMessage.substr(0, currentStatus.length + 1);
+        window.status = nextMessage;
+    }
+
+}
+
+window.setInterval(scroll, 150);
---
+++ src/PresentationSamples/OrchardCustomization/Lolcats.Caption/Models/Kitteh.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement.Records;
+using Orchard.ContentManagement;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lolcats.Caption.Models {
+    
+    public class KittehPartRecord : ContentPartRecord {
+
+        public virtual string ImageSource { get; set; }
+
+        public virtual string TextTop { get; set; }
+
+        public virtual string TextMiddle { get; set; }
+
+        public virtual string TextBottom { get; set; }
+
+    }
+
+    public class KittehPart : ContentPart<KittehPartRecord> {
+
+        [Required]
+        public string ImageSource {
+            get { return Record.ImageSource; }
+            set { Record.ImageSource = value; }
+        }
+
+        [Required]
+        public string TextTop {
+            get { return Record.TextTop; }
+            set { Record.TextTop = value; }
+        }
+
+        public string TextMiddle {
+            get { return Record.TextMiddle; }
+            set { Record.TextMiddle = value; }
+        }
+
+        [Required]
+        public string TextBottom {
+            get { return Record.TextBottom; }
+            set { Record.TextBottom = value; }
+        }
+
+    }
+}
