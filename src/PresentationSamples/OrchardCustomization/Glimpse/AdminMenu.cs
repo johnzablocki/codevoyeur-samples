a757a2b2a53d53c20a83ff41642d874df74498be
johnzablocki <johnzablocki@localhost>
2011-10-02 18:17:41 -0400

    Start of Glimpse module

---
+++ src/PresentationSamples/OrchardCustomization/Glimpse/AdminMenu.cs
@@ -0,0 +1,21 @@
+﻿using Orchard.Localization;
+using Orchard.UI.Navigation;
+using Orchard.Core.Navigation;
+
+namespace Glimpse {
+    public class AdminMenu : INavigationProvider {
+        public Localizer T { get; set; }
+
+        public AdminMenu() {
+            T = NullLocalizer.Instance;
+        }
+
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder.Add(T("Glimpse"), "7",
+                    menu => menu.Add(T("Glimpse"), "0", item => item.Action("Index", "Admin", new { area = "Glimpse" })
+                        .Permission(Permissions.ManageMainMenu)));
+        }
+    }
+}
---
+++ src/PresentationSamples/OrchardCustomization/Glimpse/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Glimpse.Models;
+using System.Configuration;
+
+namespace Glimpse.Controllers {
+    public class AdminController : Controller {
+        
+        public ActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(GlimpseSettingsPart part) {
+                        
+
+        }
+
+    }
+}
