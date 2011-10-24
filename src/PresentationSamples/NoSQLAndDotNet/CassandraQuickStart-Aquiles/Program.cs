294a565db95164fc1f2be15d46b45842261cd0f9
John Zablocki <jzablocki@users.noreply.github.com>
2011-10-24 17:25:34 -0400

    Skelatons of Cassandra projects

---
+++ src/PresentationSamples/NoSQLAndDotNet/CassandraQuickStart-Aquiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apache.Cassandra;
+using Aquiles.Cassandra10;
+
+namespace CassandraQuickStart_Aquiles
+{
+    class Program
+    {
+        private const string KEYSPACE = "cazunedra";
+
+        static void Main(string[] args)
+        {
+            doSetup();
+        }
+
+        private static void doSetup()
+        {
+            AquilesHelper.Initialize();
+
+            var cluster = AquilesHelper.RetrieveCluster("TestCluster");
+
+            var keyspaceDef = new KsDef();
+            keyspaceDef.Name = "Cazunedra";
+
+            var result = cluster.Execute(new ExecutionBlock(c => c.recv_system_drop_keyspace()));
+
+
+        }
+    }
+}
