73962a24f835e341855f2b9e34cfd658baf5a8d1
johnzablocki <johnzablocki@localhost>
2011-10-24 17:25:34 -0400

    Skelatons of Cassandra projects

---
+++ src/PresentationSamples/NoSQLAndDotNet/CassandraQuickStart-CassandraSharp/Artist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CassandraQuickStart_CassandraSharp
+{
+    public class Artist {
+
+        public string Name { get; set; }
+
+        public int Ratings { get; set; }
+
+        private IList<double> _nextShowLocation = new List<double>(2);
+
+        public IList<double> NextShowLocation {
+            get { return _nextShowLocation; }
+            set { _nextShowLocation = value; }
+        }
+        
+
+        private IList<string> _albums = new List<string>(0);
+
+        public IList<string> Albums {
+            get { return _albums; }
+            set { _albums = value; }
+        }
+
+        private IList<string> _tags = new List<string>(0);
+        
+        public IList<string> Tags {
+            get { return _tags; }
+            set { _tags = value; }
+        }        
+        
+    }
+}
---
+++ src/PresentationSamples/NoSQLAndDotNet/CassandraQuickStart-CassandraSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CassandraSharp;
+using CassandraSharp.Commands;
+using CassandraSharp.Config;
+
+namespace CassandraQuickStart_CassandraSharp
+{
+    class Program
+    {
+        private const string KEYSPACE = "Cazunedra";
+        private const string COLUMN_FAMILY = "Artists";
+
+        static void Main(string[] args)
+        {
+            doSetup();
+
+        }
+
+        private static void doSetup()
+        {
+            XmlConfigurator.Configure();
+            
+            using (var cluster = ClusterManager.GetCluster("TestCluster"))
+            {
+            }
+        }
+
+    }
+}
