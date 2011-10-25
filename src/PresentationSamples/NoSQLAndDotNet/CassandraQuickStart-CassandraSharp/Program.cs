6738d58c5f727dc98081ccc5f8034fa8fab8bad0
johnzablocki <johnzablocki@localhost>
2011-10-24 23:39:44 -0400

    Basic demo for Cassandra-Sharp

--- src/PresentationSamples/NoSQLAndDotNet/CassandraQuickStart-CassandraSharp/Program.cs
+++ src/PresentationSamples/NoSQLAndDotNet/CassandraQuickStart-CassandraSharp/Program.cs
@@ -5,28 +5,86 @@ using System.Text;
 using CassandraSharp;
 using CassandraSharp.Commands;
 using CassandraSharp.Config;
+using CassandraSharp.Model;
+using Apache.Cassandra;
 
-namespace CassandraQuickStart_CassandraSharp
-{
-    class Program
-    {
-        private const string KEYSPACE = "Cazunedra";
-        private const string COLUMN_FAMILY = "Artists";
+namespace CassandraQuickStart_CassandraSharp {
+    class Program {
+        
+        private const string KEYSPACE = "demosharp";
+        private const string COLUMN_FAMILY = "artists";
 
-        static void Main(string[] args)
-        {
-            doSetup();
+        private const string COMPARATOR_TYPE_UTF8 = "UTF8Type";
+        private const string COMPARATOR_TYPE_TIME_UUID = "TimeUUIDType";
 
-        }
 
-        private static void doSetup()
-        {
+        private static readonly ICluster _cluster;
+
+        static Program() {
+
             XmlConfigurator.Configure();
-            
-            using (var cluster = ClusterManager.GetCluster("TestCluster"))
-            {
-            }
+            _cluster = ClusterManager.GetCluster("TestCluster");
         }
 
+        static void Main(string[] args) {
+
+            doSetUp();
+
+            doColumnDef();
+
+            doInsert();
+
+            doQuery();
+
+            doTearDown();
+        }
+
+
+        private static void doSetUp() {
+
+            try {
+                _cluster.ExecuteCql("DROP KEYSPACE " + KEYSPACE);
+            } catch {                
+            }
+
+            _cluster.ExecuteCql(@"CREATE KEYSPACE " + KEYSPACE +" WITH strategy_class='SimpleStrategy' AND strategy_options:replication_factor=1;");
+            _cluster.ExecuteCql("USE " + KEYSPACE);            
+
+        }
+
+        private static void doColumnDef() {
+
+            try {
+                _cluster.DropColumnFamily(COLUMN_FAMILY);
+            } catch {
+            }
+
+            var cfDef = new CfDef {
+                Name = COLUMN_FAMILY,
+                Keyspace = KEYSPACE,
+                Comparator_type = COMPARATOR_TYPE_UTF8,
+                Comment = "Used for storing artist information"
+            };
+
+            _cluster.AddColumnFamily(cfDef);
+        }
+
+        private static void doInsert() {
+
+            _cluster.Insert(COLUMN_FAMILY, new Utf8NameOrValue("The Fratellis"),
+                                           new Utf8NameOrValue("Genre"),
+                                           new Utf8NameOrValue("Rock"), 2, ConsistencyLevel.QUORUM);
+
+            
+        }
+
+        private static void doQuery() {
+            var genre = _cluster.Get(COLUMN_FAMILY, new Utf8NameOrValue("The Fratellis"), new Utf8NameOrValue("Genre"));
+            Console.WriteLine("Genre for The Fratellis: " + Encoding.ASCII.GetString(genre.Column.Value));            
+        }
+
+        private static void doTearDown() {
+            _cluster.Dispose();
+        }
     }
 }
