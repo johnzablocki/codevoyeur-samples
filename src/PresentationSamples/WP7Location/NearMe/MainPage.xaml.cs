8e859a771cd5c07f8612bda3deab51551cfe597b
John Zablocki <jzablocki@users.noreply.github.com>
2011-11-06 22:49:07 -0500

    NearMe sample

---
+++ src/PresentationSamples/WP7Location/NearMe/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+using System.Device.Location;
+
+namespace NearMe {
+    public partial class MainPage : PhoneApplicationPage {
+        // Constructor
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e) {
+            var task = new BingMapsTask();
+            task.Center = new GeoCoordinate(42.3593, -71.0392);
+            task.SearchTerm = SearchBox.Text;
+            task.ZoomLevel = 10;
+            task.Show();
+        }
+    }
+}
