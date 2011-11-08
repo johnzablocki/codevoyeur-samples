ba38287e8190da453eda75b4f6343b68b486d877
John Zablocki <jzablocki@users.noreply.github.com>
2011-11-08 17:55:53 -0500

    Working demo with history and writes to node demo

---
+++ src/PresentationSamples/WP7Location/ParkingAssistant/ViewModels/ParkingSpot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+
+namespace ParkingAssistant.ViewModels
+{
+    [DataContract]
+    public class ParkingSpot
+    {
+        [DataMember]
+        public double Long { get; set; }
+        
+        [DataMember]
+        public double Lat { get; set; }        
+    }
+}
